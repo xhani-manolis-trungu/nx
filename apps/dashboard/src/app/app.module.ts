969d1e2ea3c7ea31050e1816777b17103af59174
xhani-manolis-trungu <trmanolis@gmail.com>
2021-04-06 23:39:35 +0300

    Frontend Masters Initial NX workspace workshop

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { CoreDataModule, coreDataRoutes } from '@production-angular/core-data';
+import {
+  CoreStateModule,
+  coreStateRoutes,
+} from '@production-angular/core-state';
+import { MaterialModule, materialRoutes } from '@production-angular/material';
+import { RoutingModule } from './routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, {}),
+    CoreDataModule,
+    CoreStateModule,
+    MaterialModule,
+    RoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
